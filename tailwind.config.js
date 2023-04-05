19b4d34c1526fffafb02cc7877054d30662b0afa
Zeynep Holat <zeynephlt7@gmail.com>
2023-04-06 00:54:06 +0300

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"], //belirtilen klasör altındaki bütün klasörleri (html,css vs) kapsa
   theme: {
-    extend: {},
+    //tüm container ler için default özellikler
+    container:{
+      center:true,
+    },
+    extend: {
+      //kendi kullanmak istediğimiz fontlar
+      fontFamily:{
+        gemunu:['Gemunu Libre', 'sans-serif'],
+        open:['Open Sans', 'sans-serif']
+      },
+      colors:{
+        'tgray':'#444353',
+        'tblue':'#00aaf6'
+      }
+    },
   },
   plugins: [],
 }
