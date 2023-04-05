f6ef560fa8c70f45a7e37aef08cd026d09cccf4d
Zeynep Holat <zeynephlt7@gmail.com>
2023-04-06 00:12:40 +0300

    tailwind-project start files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"], //belirtilen klasör altındaki bütün klasörleri (html,css vs) kapsa
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
